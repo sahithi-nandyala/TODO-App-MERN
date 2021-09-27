1ea1098d0bfe20fd6b31373949c89aa4907f94a1
sahithi_nandyala <nandyala.sahithi@gmail.com>
2021-09-27 03:47:47 -0400

    create todo UI

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+export default class TodosList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to TodosList Component!</p>
+      </div>
+    );
+  }
+}
